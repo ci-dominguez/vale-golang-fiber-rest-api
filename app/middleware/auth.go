1a5a203a2b07f55596246c5f2903407adc76ea3a
Cristian <cdom127@wgu.edu>
2024-12-12 22:32:02 -0800

    Update middleware to get user's db id from the clerk id key.

--- app/middleware/auth.go
+++ app/middleware/auth.go
@@ -2,6 +2,8 @@ package middleware
 
 import (
 	"context"
+	"github.com/ci-dominguez/vale-backend/app/models"
+	"github.com/ci-dominguez/vale-backend/database"
 	"github.com/clerk/clerk-sdk-go/v2"
 	"github.com/clerk/clerk-sdk-go/v2/jwt"
 	"github.com/gofiber/fiber/v2"
@@ -19,19 +21,25 @@ func AuthMiddleware(c *fiber.Ctx) error {
 	}
 
 	token := authHeader[len("Bearer "):]
+	println("Received Token (middleware):", token)
 
 	sessionClaims, err := jwt.Verify(context.Background(), &jwt.VerifyParams{
 		Token: token,
 	})
 	if err != nil {
+		println("JWT Verification Error:", err.Error())
 		return c.Status(fiber.StatusUnauthorized).JSON(fiber.Map{"error": "Invalid or expired token"})
 	}
 
-	userID := sessionClaims.Subject
-	if userID == "" {
-		return c.Status(fiber.StatusUnauthorized).JSON(fiber.Map{"error": "User ID not found in token claims"})
+	clerkID := sessionClaims.Subject
+	println("Extracted clerk ID (middleware):", clerkID)
+
+	var user models.User
+	if err := database.DB.Where("clerk_id = ?", clerkID).First(&user).Error; err != nil {
+		return c.Status(fiber.StatusUnauthorized).JSON(fiber.Map{"error": "User not found"})
 	}
 
-	c.Locals("userId", userID)
+	// Store user db id as string
+	c.Locals("userId", user.UserID.String())
 	return c.Next()
 }
