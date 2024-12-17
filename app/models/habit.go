90bc059905a11852859146b31592141155e4e2bc
Cristian <cdom127@wgu.edu>
2024-12-16 20:48:44 -0800

    Update habit struct and migrate habits to database. New goal and achieved fields to track habit progress per month

--- app/models/habit.go
+++ app/models/habit.go
@@ -8,10 +8,11 @@ import (
 
 // Habit represents a habit entity.
 type Habit struct {
-	HabitID          uuid.UUID `gorm:"type:uuid;default:uuid_generate_v4();primaryKey" json:"habit_id"`
-	UserID           uuid.UUID `gorm:"type:uuid;not null" json:"user_id"`
-	Name             string    `gorm:"type:varchar(255);not null" json:"name"`
-	Description      string    `gorm:"type:text" json:"description,omitempty"`
-	TotalCompletions int       `gorm:"default:0" json:"total_completions"`
-	CreatedAt        time.Time `gorm:"autoCreateTime" json:"created_at"`
+	HabitID     uuid.UUID `gorm:"type:uuid;default:uuid_generate_v4();primaryKey" json:"habit_id"`
+	UserID      uuid.UUID `gorm:"type:uuid;not null" json:"user_id"`
+	Name        string    `gorm:"type:varchar(255);not null" json:"name"`
+	Description string    `gorm:"type:text" json:"description,omitempty"`
+	Goal        int       `gorm:"default:0" json:"goal"`
+	Achieved    int       `gorm:"default:0" json:"achieved"`
+	CreatedAt   time.Time `gorm:"autoCreateTime" json:"created_at"`
 }
